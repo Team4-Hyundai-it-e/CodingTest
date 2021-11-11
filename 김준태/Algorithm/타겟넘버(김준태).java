6479cc7a9c6e9fd0984294da1dc8439505f58656
JunTae Kim <emflaxl100@naver.com>
2021-11-11 15:03:13 +0900

    #2

--- 김준태/Algorithm/타겟넘버(김준태).java
+++ 김준태/Algorithm/타겟넘버(김준태).java
@@ -1,1 +1,32 @@
-test 입니다.
+class Solution {
+    static boolean visited[];
+    static int answer = 0;
+    
+    public int solution(int[] numbers, int target) {
+        
+        visited = new boolean[numbers.length];
+        
+        // dfs 및 카운트 증가
+        dfs(0, numbers, 0, target);
+        
+        return answer;
+    }
+    
+    static void dfs(int depth, int[] numbers, int sum, int target){
+        // 종료조건
+        if (depth == numbers.length){
+            if(sum == target){
+                answer++;
+            }
+            return;
+        }
+        if (visited[depth]){
+            return;
+        }
+        // 재귀
+        visited[depth] = true;
+        dfs(depth + 1, numbers, sum + numbers[depth], target);
+        dfs(depth + 1, numbers, sum - numbers[depth], target);
+        visited[depth] = false;
+    }
+}
